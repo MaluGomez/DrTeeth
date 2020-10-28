e04e3fd5408e42debea48b6a3ea3350d4b768a0c
MaluGomez <malugomezr.mgr@gmail.com>
2020-10-28 15:24:32 -0600

    DrTeeth1

---
+++ source/src/components/partials/AppSidebar.vue
@@ -0,0 +1,139 @@
+<template lang="html">
+  <section class="app-sidebar">
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <ul class="nav">   
+        <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+          
+         </div>  
+        <li class="nav-item nav-category"></li>
+        <li class="nav-item">
+          <a class="nav-link" v-b-toggle="'dashboard-dropdown'">
+            <i class="menu-icon typcn typcn-document-text"></i>
+            <router-link class="nav-link" to="/">
+            <span class="menu-title">Perfil</span>
+            </router-link>
+            <i class="menu-arrow"></i>
+          </a>
+          
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-b-toggle="'ui-basic'">
+            <i class="menu-icon typcn typcn-coffee"></i>
+            <span class="menu-title">Pacientes</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <b-collapse id="ui-basic">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/buttons/">Lista de Pacientes</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/dropdowns/">Nueva Consulta</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/typography/">Nuevo Paciente</router-link>
+              </li>
+            </ul>
+          </b-collapse>
+        </li>
+        
+         <li class="nav-item">
+          <span class="nav-link" v-b-toggle="'charts-dropdown'">
+            <i class="menu-icon typcn typcn-th-large-outline"></i>
+            <span class="menu-title">Agenda</span>
+            <i class="menu-arrow"></i>
+          </span>
+          <b-collapse id="charts-dropdown">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/chartjs/">Citas</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/registro/">Nueva Cita</router-link>
+              </li>
+             
+                             
+            </ul>
+            
+          </b-collapse>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-b-toggle="'tables'">
+            <i class="menu-icon mdi mdi-table"></i>
+            <span class="menu-title">Reportes</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <b-collapse id="tables">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/basic_table/">Basic Table</router-link>
+              </li>
+            </ul>
+          </b-collapse>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-b-toggle="'icons'">
+            <i class="menu-icon typcn typcn-user-outline"></i>
+            <span class="menu-title">Icons</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <b-collapse id="icons">
+            <ul class="nav flex-column sub-menu">
+               <li class="nav-item">
+                <router-link class="nav-link" to="/mdiIcons/">MDI</router-link>
+              </li>
+            </ul>
+          </b-collapse>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-b-toggle="'auth'">
+            <i class="menu-icon typcn typcn-document-add"></i>
+            <span class="menu-title">User Pages</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <b-collapse id="auth">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/pages/login/">Login</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/pages/register/">Register</router-link>
+              </li>
+            </ul>
+          </b-collapse>
+        </li>
+       
+      </ul>
+    </nav>
+  </section>
+</template>
+
+<script lang="js">
+import JQuery from 'jquery'
+// eslint-disable-next-line
+let $ = JQuery
+export default {
+  name: 'app-sidebar',
+  mounted () {
+    const body = document.querySelector('body')
+    // add class 'hover-open' to sidebar navitem while hover in sidebar-icon-only menu
+    document.querySelectorAll('.sidebar .nav-item').forEach(function (el) {
+      el.addEventListener('mouseover', function () {
+        if (body.classList.contains('sidebar-icon-only')) {
+          el.classList.add('hover-open')
+        }
+      })
+      el.addEventListener('mouseout', function () {
+        if (body.classList.contains('sidebar-icon-only')) {
+          el.classList.remove('hover-open')
+        }
+      })
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.app-sidebar {
+}
+</style>
---
+++ source/src/views/charts/registro.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+
+  <section class="registro">
+    <div class="row">
+      <div class="col-md-6 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script lang="js">
+export default {
+  name: 'registro'
+}
+</script>
+
+<style scoped lang="scss">
+.registro {
+
+}
+</style>
